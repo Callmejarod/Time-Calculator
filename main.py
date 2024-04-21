1ea7ce0b10d6330cef346d3fa0e36eb06ba892ed
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2024-04-21 12:22:22 -0700

    Added commnets for code readability

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ def add_time(start, duration, day=''):
 
     
 
-
+    #Splits start into list
     start_list = start.split(":")
     start_list += start_list[1].split(" ")
     start_list.pop(1)
     print(start_list)
 
 
-
+    #Splits duration into list
     duration_list = duration.split(":")
     print(duration_list)
 
@@ -21,6 +21,7 @@ def add_time(start, duration, day=''):
     initial_am_pm = str(start_list[2])
     new_pm_am = str(start_list[2])
   
+    #Handles AM and PM
     if new_hour >= 12:
         if new_pm_am == 'PM':
             new_pm_am = 'AM'
@@ -30,13 +31,16 @@ def add_time(start, duration, day=''):
             print(new_pm_am)
 
 
+    #Handles minutes
     if new_minutes > 59:
         new_hour += 1
         new_minutes -= 60
 
+    #Handles hours
     if new_hour > 12:
         new_hour %= 12
     
+    #Handles single digit minutes 
     if new_minutes < 10:
         new_minutes = str(new_minutes).rjust(2, "0")
 
@@ -44,8 +48,8 @@ def add_time(start, duration, day=''):
 
 
 
-
-
+    #(Work in progress)
+    #Handles moving thorughout days of the week 
     days_of_the_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
     if day.lower() in days_of_the_week:
@@ -60,12 +64,13 @@ def add_time(start, duration, day=''):
         
 
 
-
+    #Formatted string that shows outpout
     new_time = (f'{new_hour}:{new_minutes} {new_pm_am} {day.capitalize()}')
 
 
     return new_time
 
+#Calls add_time function
 print(add_time('11:43 PM', '1:17', 'tueSday'))
 
 
