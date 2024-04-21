c281e3524f50ca47401dd501269d459e2e42c59a
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2024-04-20 23:20:03 -0700

    First attempt for counting days

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def add_time(start, duration, day=''):
 
-    days_of_the_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    
 
 
     start_list = start.split(":")
@@ -15,15 +15,11 @@ def add_time(start, duration, day=''):
     duration_list = duration.split(":")
     print(duration_list)
 
-
+    initial_hour = int(start_list[0]) + int(duration_list[0])
     new_hour = int(start_list[0]) + int(duration_list[0])
     new_minutes = int(start_list[1]) + int(duration_list[1]) 
+    initial_am_pm = str(start_list[2])
     new_pm_am = str(start_list[2])
-
-
-
-    
- 
   
     if new_hour >= 12:
         if new_pm_am == 'PM':
@@ -45,14 +41,33 @@ def add_time(start, duration, day=''):
 
 
 
-    new_time = (f'{new_hour}:{new_minutes} {new_pm_am} {day}')
+
+
+
+
+    days_of_the_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+    if day.lower() in days_of_the_week:
+        if initial_hour >= 12 and initial_am_pm == 'PM':
+            index = (days_of_the_week.index(day.lower()) + 1)
+            day = days_of_the_week[index]
+
+
+              
+                 
+                
+        
+
+
+
+    new_time = (f'{new_hour}:{new_minutes} {new_pm_am} {day.capitalize()}')
 
 
     return new_time
 
-print(add_time('10:00 PM', '12:05','tuesday'))
+print(add_time('11:43 PM', '3:00', 'tueSday'))
 
 
 #FIX: Changes throughout the days of the week 
-#FIX: Single minutes digits error 
+
 
